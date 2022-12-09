7fb43cc264f14aefe91d9f017d2ccae46052a1f4
Alan Pecenkovic <alanquad@yahoo.com>
2022-12-09 16:21:03 +0100

    getting a place by user id

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -26,4 +26,14 @@ router.get("/:pid", (req, res, next) => {
   });
 });
 
+router.get("/user/:uid", (req, res, next) => {
+  const userId = req.params.uid;
+  const place = DUMMY_PLACES.find((p) => {
+    return p.creator === userId;
+  });
+  res.json({
+    place,
+  });
+});
+
 module.exports = router;
