f4f80c46f75157c3ad1a019df9a76831909e4eae
Alan Pecenkovic <alanquad@yahoo.com>
2022-12-14 13:37:33 +0100

    creating and storing documents in the database

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -3,6 +3,7 @@ const { validationResult } = require("express-validator");
 
 const HttpError = require("../models/http-error");
 const getCoordinatesForAddress = require("../util/location");
+const Place = require("../models/place");
 
 let DUMMY_PLACES = [
   {
@@ -68,16 +69,22 @@ const createPlace = async (req, res, next) => {
     return next(error);
   }
 
-  const createdPlace = {
-    id: uuidv4(),
+  const createdPlace = new Place({
     title,
     description,
-    location: coordinates,
     address,
+    location: coordinates,
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Empire_State_Building_%28aerial_view%29.jpg/800px-Empire_State_Building_%28aerial_view%29.jpg",
     creator,
-  };
+  });
 
-  DUMMY_PLACES.push(createdPlace);
+  try {
+    createdPlace.save();
+  } catch (err) {
+    const error = new HttpError("Failed to create a place", 500);
+    return next(error);
+  }
 
   res.status(201).json({ place: createdPlace });
 };
