80a30a046886b679b080f466c18783aa3968ab9d
Alan Pecenkovic <alanquad@yahoo.com>
2022-12-19 15:17:52 +0100

    handling CORS errors

--- app.js
+++ app.js
@@ -10,6 +10,16 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-Width,Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE");
+  next();
+});
+
 app.use("/api/places", placesRoutes);
 app.use("/api/users", userRoutes);
 
@@ -30,7 +40,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    "mongodb+srv://test:test123@cluster0.1tsjj.mongodb.net/places?retryWrites=true&w=majority"
+    "mongodb+srv://test:test123@cluster0.1tsjj.mongodb.net/mern?retryWrites=true&w=majority"
   )
   .then(() => {
     app.listen(5000);
--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,17 +1,7 @@
-const { v4: uuidv4 } = require("uuid");
 const { validationResult } = require("express-validator");
 
 const HttpError = require("../models/http-error");
 const User = require("../models/user");
-
-const DUMMY_USERS = [
-  {
-    id: "u1",
-    name: "John Smith",
-    email: "test@test.com",
-    password: "test123",
-  },
-];
 
 const getUsers = async (req, res, next) => {
   let users;
