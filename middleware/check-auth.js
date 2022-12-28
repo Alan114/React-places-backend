760f90187e7168a5c5ca4dbf9da50836f2ace8a6
Alan Pecenkovic <alanquad@yahoo.com>
2022-12-28 13:26:40 +0100

    allowing pre-flight request and adding places

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -2,14 +2,17 @@ const jwt = require("jsonwebtoken");
 const httpError = require("../models/http-error");
 
 module.exports = (req, res, next) => {
+  if (req.method === "OPTIONS") {
+    return next();
+  }
   try {
-    const token = req.headers.authorization.split("")[1]; // Authorization: 'Bearer TOKEN'
+    const token = req.headers.authorization.split(" ")[1]; // Authorization: 'Bearer TOKEN'
     if (!token) {
       throw new Error("Authentication failed!");
-      const decodedToken = jwt.verify(token, "supersecret_hidden");
-      req.userData = { userId: decodedToken.userId };
-      next();
     }
+    const decodedToken = jwt.verify(token, "supersecret_hidden");
+    req.userData = { userId: decodedToken.userId };
+    next();
   } catch (err) {
     const error = new httpError("Authentication failed!", 401);
     return next(error);
